fb57be5a73602a1933d731da113d65a6d8e3e348
Allen Chrisso Dhas <allencdvja@gmail.com>
2023-10-31 23:59:49 +0530

    first commit

---
+++ descord.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+
+// Create a new Discord client
+const client = new Discord.Client({
+  intents: [Discord.GatewayIntentBits.GUILDS],
+});
+
+// Login to Discord
+client.login(process.env.TOKEN);
+
+// Handle the 'ready' event
+client.on("ready", () => {
+  console.log("Logged in as ${client.user.tag}");
+});
+
+// Handle the 'message' event
+client.on("message", (message) => {
+  // If the message is from the bot itself, ignore it
+  if (message.author.bot) return;
+
+  // Respond to the message with a simple greeting
+  message.channel.send("Hello, ${message.author.username}!");
+});
