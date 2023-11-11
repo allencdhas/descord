45a96128f15d930db4f4ecbbe5836e7881ad745d
Allen Chrisso Dhas <allencdvja@gmail.com>
2023-11-11 16:10:00 +0530

    Retrieve command files -> descord.js

--- descord.js
+++ descord.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 
 const { fs } = require("node:fs");
 const { path } = require("node:path");
-
 const { Client, Events, GatewayIntentBits } = require("discord.js");
-
 const Discord = require("discord.js");
 const { Collection } = require("discord.js");
 
@@ -13,6 +11,24 @@ const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds] });
 
 //Create command collection
 client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+//Retrieve command files...
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
 
 // Login to Discord
 client.login(process.env.TOKEN);
